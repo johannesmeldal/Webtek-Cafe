9a33e7ccc37e52407e1b77e336bdf6717bc0a1f1
Elin Cathrine Bjune Bjerve <ecbjerve@ntnu.no>
2021-10-27 15:20:16 +0200

    la inn bilde til arrangement

---
+++ js/arrangementer.js
@@ -0,0 +1,24 @@
+function lesMer() { 
+    var dotter = document.getElementById('dotter');
+    var merText = document.getElementById('mer');
+    var knapp_lesText = document.getElementById('knapp_les');
+    const del1 = document.getElementById("del1");
+
+    if (knapp_lesText. onclick){
+        del1.innerHTML += "hei det er meg";
+    }
+
+
+
+   /* if (dotter.style.display == none) { 
+        dotter.style.display = 'inline';
+        knapp_lesText.innerHTML = 'Les mer';
+        merText.style.display = 'none';
+    } 
+    else { 
+        dotter.style.display ='none';
+        knapp_lesText.innerHTML = 'Les mindre';
+        merText.style.display = 'inline';
+    }*/
+}
+
