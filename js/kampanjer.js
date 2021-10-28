b60bf95ca3c31da66fc8ddb99c58412eab9f5e14
August SKarsfjord Nyheim <augustsn@stud.ntnu.no>
2021-10-28 14:32:38 +0200

    div styling på kampanjer og arrangementer

--- js/kampanjer.js
+++ js/kampanjer.js
@@ -2,7 +2,7 @@ const bilde1 = document.getElementById("bilde1");
 const bilde2 = document.getElementById("bilde2");
 
 function click_one(){
-    //something
+    window.location.href = 'meny.html#drikker';
 }
 
 
