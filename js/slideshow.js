34a1d87410556a7d9e55a102b05dce251a1d0e99
anineolafsen <90967507+anineolafsen@users.noreply.github.com>
2021-11-02 17:38:25 +0100

    Format document

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,43 +1,43 @@
 var slides = document.getElementsByClassName("slide");
-        var dots = document.getElementsByClassName("dot");
-        var currentSlide = 0;
-        var loop;
+var dots = document.getElementsByClassName("dot");
+var currentSlide = 0;
+var loop;
+slides[currentSlide].style.display = "block";
+slideshow();
+
+function slideshow() {
+    if (currentSlide < (slides.length - 1)) {
+        if (currentSlide == 0) {
+            slides[5].style.display = "none";
+            slides[currentSlide].style.display = "block";
+            dots[5].style.backgroundColor = "white";
+            dots[currentSlide].style.backgroundColor = "#4e765c";
+            dots[5].style.backgroundColor = "white";
+            dots[currentSlide].style.backgroundColor = "#4e765c";
+        } else {
+            slides[currentSlide - 1].style.display = "none";
+            slides[currentSlide].style.display = "block";
+            dots[currentSlide - 1].style.backgroundColor = "white";
+            dots[currentSlide].style.backgroundColor = "#4e765c";
+        }
+        currentSlide++;
+    } else {
+        slides[currentSlide - 1].style.display = "none";
         slides[currentSlide].style.display = "block";
-        slideshow();
+        dots[currentSlide - 1].style.backgroundColor = "white";
+        dots[currentSlide].style.backgroundColor = "#4e765c";
+        currentSlide = 0;
+    }
+    loop = setTimeout(slideshow, 3000)
+}
 
-        function slideshow() {
-            if (currentSlide < (slides.length-1)) {
-                if (currentSlide == 0) {
-                    slides[5].style.display = "none";
-                    slides[currentSlide].style.display = "block";
-                    dots[5].style.backgroundColor = "white";
-                    dots[currentSlide].style.backgroundColor = "#4e765c";
-                    dots[5].style.backgroundColor = "white";
-                    dots[currentSlide].style.backgroundColor = "#4e765c";
-                } else {
-                    slides[currentSlide-1].style.display = "none";
-                    slides[currentSlide].style.display = "block";
-                    dots[currentSlide-1].style.backgroundColor = "white";
-                    dots[currentSlide].style.backgroundColor = "#4e765c";
-                }
-                currentSlide++;
-            } else {
-                slides[currentSlide-1].style.display = "none";
-                slides[currentSlide].style.display = "block";
-                dots[currentSlide-1].style.backgroundColor = "white";
-                dots[currentSlide].style.backgroundColor = "#4e765c";
-                currentSlide = 0;
-            }
-            loop = setTimeout(slideshow, 3000)
-        }
+function switchSlide(n) {
 
-        function switchSlide(n) {
-            
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-                dots[i].style.backgroundColor = "white";
-            }
-            clearTimeout(loop);
-            currentSlide = n;
-            slideshow();
-        }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].style.backgroundColor = "white";
+    }
+    clearTimeout(loop);
+    currentSlide = n;
+    slideshow();
+}
