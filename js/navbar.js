8e2ab708a38f31cf06e4c26a84b3fa9102f70ab0
martin39 <martin39@stud.ntnu.no>
2021-11-05 14:08:30 +0100

    removed

--- js/navbar.js
+++ js/navbar.js
@@ -8,18 +8,3 @@ document.write(`
     </ul>
 `)
 
-
-document.addEventListener("DOMContentLoaded", function(){
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 50) {
-          document.getElementById('.nav').classList.add('fixed-top');
-          // add padding top to show content behind navbar
-          navbar_height = document.querySelector('.navbar').offsetHeight;
-          document.body.style.paddingTop = navbar_height + 'px';
-        } else {
-          document.getElementById('.nav').classList.remove('fixed-top');
-           // remove padding top from body
-          document.body.style.paddingTop = '0';
-        } 
-    });
-  }); 
