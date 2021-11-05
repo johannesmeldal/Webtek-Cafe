253edf5a06c7833ca31193ce7d7b671b33d82fab
martin39 <martin39@stud.ntnu.no>
2021-11-05 14:06:16 +0100

    nav

--- js/navbar.js
+++ js/navbar.js
@@ -7,3 +7,19 @@ document.write(`
         <li> <a class="navlink" href="omoss.html"> OM OSS </a> </li>
     </ul>
 `)
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+          document.getElementById('.nav').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('.nav').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  }); 
