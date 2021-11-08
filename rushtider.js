3259c9a785a335d87f71cce2b52eff2585c7f556
August SKarsfjord Nyheim <augustsn@stud.ntnu.no>
2021-11-08 19:41:04 +0100

    rushtider js

--- rushtider.js
+++ rushtider.js
@@ -1,49 +1,148 @@
 const days = {
-    "MAN" : {
-        "9-11" : 20,
-        "11-13" : 50,
-        "13-15" : 60,
-        "15-18" : 70,  
-        "18-20" : 60,      
+    "Mandag" : {
+        "9-11" : 1,
+        "11-13" : 2,
+        "13-15" : 2,
+        "15-18" : 3,  
+        "18-20" : 2,      
     },
-    "TIR" : {
-        "9-11" : 20,
-        "11-13" : 35,
-        "13-15" : 70,
-        "15-18" : 90,  
-        "18-20" : 65,   
+    "Tirsdag" : {
+        "9-11" : 1,
+        "11-13" : 1,
+        "13-15" : 2,
+        "15-18" : 3,  
+        "18-20" : 2,   
     },
-    "ONS" : {
-        "9-11" : 20,
-        "11-13" : 50,
-        "13-15" : 60,
-        "15-18" : 70,  
-        "18-20" : 60,      
+    "Onsdag" : {
+        "9-11" : 1,
+        "11-13" : 2,
+        "13-15" : 2,
+        "15-18" : 3,  
+        "18-20" : 2,      
     },
-    "TOR" : {
-        "9-11" : 20,
-        "11-13" : 35,
-        "13-15" : 70,
-        "15-18" : 90,  
-        "18-20" : 65,      
+    "Torsdag" : {
+        "9-11" : 1,
+        "11-13" : 1,
+        "13-15" : 2,
+        "15-18" : 3,  
+        "18-20" : 2,      
     },
-    "FRE" : {
-        "9-11" : 20,
-        "11-13" : 50,
-        "13-15" : 60,
-        "15-18" : 70,  
-        "18-20" : 60,      
+    "Fredag" : {
+        "9-11" : 1,
+        "11-13" : 2,
+        "13-15" : 2,
+        "15-18" : 3,  
+        "18-20" : 2,      
     },
-    "LØR" : {
-        "10-12" : 25,
-        "12-14" : 65,
-        "14-16" : 70,
-        "16-18" : 60,        
+    "Lørdag" : {
+        "10-12" : 1,
+        "12-14" : 2,
+        "14-16" : 3,
+        "16-18" : 2,        
     },
-    "SØN" : {
-        "10-12" : 20,
-        "12-14" : 50,
-        "14-15" : 60, 
-        "15-16" : 40,      
+    "Søndag" : {
+        "10-12" : 1,
+        "12-14" : 2,
+        "14-15" : 2, 
+        "15-16" : 1,      
     },
 }
+
+const canvas = document.getElementById('chart');
+const context = canvas.getContext('2d');
+const wCanvas = 500
+const hCanvas = 500
+const margin = 50
+const smallMargin = margin / 2
+const widthBar = 50
+
+function drawLine(fromX, fromY, toX, toY) {
+  context.beginPath();
+  context.moveTo(toX, toY);
+  context.lineTo(fromX, fromY);
+  context.stroke();
+}
+
+function drawText(text, x, y) {
+  context.fillStyle = 'black';
+  context.fillText(text, x, y);
+}
+
+function drawLineWithText(text, fromX, fromY, toX, toY) {
+  drawText(text, fromX - 35, fromY + 7);
+  drawLine(fromX, fromY, toX, toY);
+}
+
+
+// function to draw rectangles width color
+function drawRect(x, y, w, h, color) {
+  context.fillStyle = color
+  context.fillRect(x, y, w, h)
+}
+
+function drawRectWithText(x, y, w, h, color, text) {
+    drawRect(x, y, w, h, color);
+    drawText(text,x + 3,y + 20)
+}
+
+const d = new Date();
+
+const weekday = new Array(7);
+weekday[0] = "Søndag";
+weekday[1] = "Mandag";
+weekday[2] = "Tirsdag";
+weekday[3] = "Onsdag";
+weekday[4] = "Torsdag";
+weekday[5] = "Fredag";
+weekday[6] = "Lørdag";
+
+
+let day = weekday[d.getDay()];
+let t = 0;
+// function to draw the graph
+function drawGraph(Array) {
+    for (let j = 0; j < 7; j++) {
+        document.getElementById(j).style.backgroundColor = 'white';
+    }
+    if (t > 0) {
+        document.getElementById("daydiv").innerHTML = event.srcElement.innerHTML;
+        event.srcElement.style.backgroundColor = 'lightgrey';
+    }
+    
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.font = "26px Nunito"
+    
+    for (let i = 0; i < 4; i++) {
+        drawLineWithText(String(i * 1), margin, hCanvas + margin - i * (hCanvas + margin) / 4, wCanvas + margin,
+        hCanvas + margin - i * (hCanvas + margin) / 4)
+    }
+
+    context.font = "20px Nunito";
+    const length = Object.keys(Array).length
+    let i = 0
+    for (Obj in Array) {
+        const height = Array[Obj] * 138
+        x = i / (length - 1) * (wCanvas - (margin * 2)) + margin + smallMargin;
+
+        let graphColor;
+        if (Array[Obj] == 3) {
+            graphColor = 'red'
+        } else {
+            graphColor = 'blue'
+        }
+        drawRectWithText(x, hCanvas + margin, widthBar, -height, graphColor, Obj)
+        
+        i++
+        t++;
+    }
+    context.font = "26px Nunito"
+    drawText('3 - Opptatt', 565, 170);
+    drawText('2 - Middels', 565, 210);
+    drawText('1 - Rolig', 565, 250);
+}
+
+
+
+drawGraph(days[day]);
+document.getElementById("daydiv").innerHTML = day;
+document.getElementById(String(d.getDay())).style.backgroundColor = 'lightgrey';
