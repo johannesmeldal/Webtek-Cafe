f1cce370a5554b1adcf553b960cf9c32b765dea1
Elin Cathrine Bjune Bjerve <ecbjerve@ntnu.no>
2021-11-09 10:58:29 +0100

    endringer

--- js/kontaktskjemareturn.js
+++
@@ -1,9 +0,0 @@
-function seSvar() {
-    var x = document.getElementById("sendSvar");
-    var txt = "";
-    var i;
-    for (i = 0; i < x.length; i++) {
-      txt = txt + x.elements[i].value + "<br>";
-    }
-    document.getElementById("svar_kontaktskjema").innerHTML = txt;
-  }
